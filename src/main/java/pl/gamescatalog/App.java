3462f2255855958c39dc1fcd96bc14507f1d860f
s16315 <s16315@pjwstk.edu.pl>
2019-10-19 22:24:23 +0200

    puste repo

---
+++ src/main/java/pl/gamescatalog/App.java
@@ -0,0 +1,13 @@
+package pl.gamescatalog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
